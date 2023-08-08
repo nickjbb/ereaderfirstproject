96e15a3394356d762274d0e936e56cc85a3fa24a
Nicholas Battin <nickjbattin@gmail.com>
2023-08-08 12:11:03 -0500

    First Model Commit

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Genre, PDF, Author
 # Register your models here.
+
+admin.register(Genre, PDF, Author)(admin.ModelAdmin)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=100, help_text="Enter genres")
+
+    def __str__(self):
+        return self.name
+
+class PDF(models.Model):
+    title = models.CharField(max_length=250)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    genre = models.ManyToManyField(Genre, help_text="What is the document's genre?")
+
+    def __str__(self):
+        return self.title
+
+
+class Author(models.Model):
+    full_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.full_name
+
